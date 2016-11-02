5dd2c2d61b6e26e0459920500d6e4e6d1fca81ce
luxidea <contact@luxidea.net>
2016-11-02 15:56:35 +0900

    OpenCV 2.4.13 に対応。

--- FaceDetect.dpr
+++ FaceDetect.dpr
@@ -3,14 +3,14 @@
 uses
   FMX.Forms,
   Main in 'Main.pas' {Form1},
-  LUX.Vision.OpenCV in '_LIBRARY\LUX.idea\» Vision\» OpenCV\LUX.Vision.OpenCV.pas',
-  LUX.Vision.OpenCV.Detect in '_LIBRARY\LUX.idea\» Vision\» OpenCV\LUX.Vision.OpenCV.Detect.pas',
-  ocv.core_c in '_LIBRARY\LUX.idea\» Vision\» OpenCV\› Laex\Delphi-OpenCV\source\ocv.core_c.pas',
-  ocv.imgproc.types_c in '_LIBRARY\LUX.idea\» Vision\» OpenCV\› Laex\Delphi-OpenCV\source\ocv.imgproc.types_c.pas',
-  ocv.imgproc_c in '_LIBRARY\LUX.idea\» Vision\» OpenCV\› Laex\Delphi-OpenCV\source\ocv.imgproc_c.pas',
-  ocv.lib in '_LIBRARY\LUX.idea\» Vision\» OpenCV\› Laex\Delphi-OpenCV\source\ocv.lib.pas',
-  ocv.core.types_c in '_LIBRARY\LUX.idea\» Vision\» OpenCV\› Laex\Delphi-OpenCV\source\ocv.core.types_c.pas',
-  ocv.objdetect_c in '_LIBRARY\LUX.idea\» Vision\» OpenCV\› Laex\Delphi-OpenCV\source\ocv.objdetect_c.pas';
+  ocv.core_c in '_LIBRARY\LUXOPHIA\LUX.Vision.OpenCV\› Laex\Delphi-OpenCV\source\ocv.core_c.pas',
+  ocv.imgproc.types_c in '_LIBRARY\LUXOPHIA\LUX.Vision.OpenCV\› Laex\Delphi-OpenCV\source\ocv.imgproc.types_c.pas',
+  ocv.imgproc_c in '_LIBRARY\LUXOPHIA\LUX.Vision.OpenCV\› Laex\Delphi-OpenCV\source\ocv.imgproc_c.pas',
+  ocv.lib in '_LIBRARY\LUXOPHIA\LUX.Vision.OpenCV\› Laex\Delphi-OpenCV\source\ocv.lib.pas',
+  ocv.objdetect_c in '_LIBRARY\LUXOPHIA\LUX.Vision.OpenCV\› Laex\Delphi-OpenCV\source\ocv.objdetect_c.pas',
+  ocv.core.types_c in '_LIBRARY\LUXOPHIA\LUX.Vision.OpenCV\› Laex\Delphi-OpenCV\source\ocv.core.types_c.pas',
+  LUX.Vision.OpenCV in '_LIBRARY\LUXOPHIA\LUX.Vision.OpenCV\LUX.Vision.OpenCV.pas',
+  LUX.Vision.OpenCV.Detect in '_LIBRARY\LUXOPHIA\LUX.Vision.OpenCV\LUX.Vision.OpenCV.Detect.pas';
 
 {$R *.res}
 
